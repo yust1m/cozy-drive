56421e1d772a6e0abb50ffcdda36976a3c8818d4
Patrick Browne <pt.browne@gmail.com>
2020-06-29 09:31:11 +0200

    refactor: Extract function and set useCallback dependencies

--- src/drive/web/modules/views/Drive/RealTimeQueries.jsx
+++ src/drive/web/modules/views/Drive/RealTimeQueries.jsx
@@ -2,54 +2,58 @@ import { useEffect } from 'react'
 import { useClient, Mutations } from 'cozy-client'
 import { receiveMutationResult } from 'cozy-client/dist/store'
 
+const dispatchChange = (
+  document,
+  mutationDefinitionCreator,
+  client,
+  doctype
+) => {
+  const response = { data: { ...document, _type: doctype } }
+  const options = {}
+  client.dispatch(
+    receiveMutationResult(
+      client.generateId(),
+      response,
+      options,
+      mutationDefinitionCreator(document)
+    )
+  )
+}
+
 const RealTimeQueries = ({ doctype }) => {
   const client = useClient()
 
-  const dispatchChange = (document, mutationDefinitionCreator) => {
-    const response = { data: { ...document, _type: doctype } }
-    const options = {}
-    client.dispatch(
-      receiveMutationResult(
-        client.generateId(),
-        response,
-        options,
-        mutationDefinitionCreator(document)
+  useEffect(() => {
+    const realtime = client.plugins.realtime
+
+    const dispatchCreate = document => {
+      dispatchChange(document, Mutations.createDocument, client, doctype)
+    }
+    const dispatchUpdate = document => {
+      dispatchChange(document, Mutations.updateDocument, client, doctype)
+    }
+    const dispatchDelete = document => {
+      dispatchChange(
+        { ...document, _deleted: true },
+        Mutations.deleteDocument,
+        client,
+        doctype
       )
-    )
-  }
+    }
 
-  useEffect(
-    () => {
-      const realtime = client.plugins.realtime
+    const subscribe = async () => {
+      await realtime.subscribe('created', doctype, dispatchCreate)
+      await realtime.subscribe('updated', doctype, dispatchUpdate)
+      await realtime.subscribe('deleted', doctype, dispatchDelete)
+    }
+    subscribe()
 
-      const dispatchCreate = document => {
-        dispatchChange(document, Mutations.createDocument)
-      }
-      const dispatchUpdate = document => {
-        dispatchChange(document, Mutations.updateDocument)
-      }
-      const dispatchDelete = document => {
-        dispatchChange(
-          { ...document, _deleted: true },
-          Mutations.deleteDocument
-        )
-      }
-
-      const subscribe = async () => {
-        await realtime.subscribe('created', doctype, dispatchCreate)
-        await realtime.subscribe('updated', doctype, dispatchUpdate)
-        await realtime.subscribe('deleted', doctype, dispatchDelete)
-      }
-      subscribe()
-
-      return () => {
-        realtime.unsubscribe('created', doctype, dispatchCreate)
-        realtime.unsubscribe('updated', doctype, dispatchUpdate)
-        realtime.unsubscribe('deleted', doctype, dispatchDelete)
-      }
-    },
-    [client, dispatchChange]
-  )
+    return () => {
+      realtime.unsubscribe('created', doctype, dispatchCreate)
+      realtime.unsubscribe('updated', doctype, dispatchUpdate)
+      realtime.unsubscribe('deleted', doctype, dispatchDelete)
+    }
+  }, [client, doctype])
 
   return null
 }
